c8d5ce30b9799108d672ebfa303c5ae1cc9778f2
Abhishek Srivastava <53976003+abhishek2x@users.noreply.github.com>
2020-09-08 01:48:25 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,36 +1,38 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Detail from "./pages/Detail";
 import Home from "./pages/Home";
 import Toast from "./pages/Toast";
-
+import Navbar from "./src/Navbar";
 const Stack = createStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ title: "Home" }}
-        />
+    <>
+      <Navbar />
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{ title: "Home" }}
+          />
 
-        <Stack.Screen
-          name="Detail"
-          component={Detail}
-          options={{ title: "Details " }}
-        />
+          <Stack.Screen
+            name="Detail"
+            component={Detail}
+            options={{ title: "Details " }}
+          />
 
-        <Stack.Screen
-          name="Toast"
-          component={Toast}
-          options={{ title: "Toast " }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+          <Stack.Screen
+            name="Toast"
+            component={Toast}
+            options={{ title: "Toast " }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
